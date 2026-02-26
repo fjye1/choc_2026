8ba0db589e6fba89ea6de830e80127925fb67cd6
Frederick Elledge <frederickjye@gmail.com>
2026-02-26 12:57:33 +0000

    Split CSS into seperate files based on usecase update path to static in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,11 @@ from flask_login import current_user
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder="../templates")
+    app = Flask(
+        __name__,
+        static_folder='../static',  # make sure path points to your project static/
+        template_folder='../templates'
+    )
     app.config.from_object("config.Config")
 
     db.init_app(app)
